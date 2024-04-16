23458d788f2bed233505f86b1cbda1fe25f25c3a
jinoc <choiae369@gmail.com>
2024-04-17 01:01:14 +0900

    Feat: 상품 타입 설정 및 API 함수 return type 설정

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,6 +1,8 @@
+import { ProductListResType } from '../types/aboutProduct';
+
 export const getAllProduct = async () => {
   const res = await fetch('https://dummyjson.com/products');
-  const productList = await res.json();
+  const productList: ProductListResType = await res.json();
 
   return productList;
 };
